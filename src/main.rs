4edadeaccfb1565094f97f50a0d0e24f9126fd51
SF-Zhou <sfzhou.scut@gmail.com>
2024-10-27 15:43:49 +0000

    upgrade to 0.1.1
    1. support parallel reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use std::fs;
-use std::io::Read;
-use std::path::PathBuf;
+use scoped_threadpool::Pool;
+use std::cell::RefCell;
+use std::fs::{self, File};
+use std::os::unix::fs::FileExt;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
@@ -10,29 +12,70 @@ struct Args {
     /// file path. If not specified, read from stdin.
     paths: Vec<PathBuf>,
 
-    /// read batch size.
-    #[arg(short, long, default_value_t = 16 << 20)]
-    batch_size: usize,
+    /// number of threads.
+    #[arg(short, long, default_value_t = 1)]
+    threads: u32,
+}
+
+const BLOCK_SIZE: u64 = 16 << 20; // 16MiB.
+thread_local! {
+    static TLS: RefCell<Vec<u8>> = RefCell::new(vec![0; BLOCK_SIZE as _]);
+}
+
+fn parallel_read(file: &File, path: &Path, pool: &mut Pool) -> Result<u32> {
+    let mut start = 0u64;
+    let mut crc32c = 0u32;
+    loop {
+        let mut vec: Vec<Result<(u64, u32, bool)>> = vec![];
+        vec.resize_with(pool.thread_count() as usize, || Ok((0, 0, true)));
+
+        pool.scoped(|scoped| {
+            for (i, r) in vec.iter_mut().enumerate() {
+                scoped.execute(move || {
+                    let offset = start + i as u64 * BLOCK_SIZE;
+                    *r = TLS.with(|v| -> Result<(u64, u32, bool)> {
+                        let mut buf = v.borrow_mut();
+                        let n = file.read_at(&mut buf, offset).with_context(|| {
+                            format!("read source file failed: {}", path.display())
+                        })?;
+                        let crc32c = crc32c::crc32c(&buf[..n]);
+                        Ok((n as u64, crc32c, n as u64 != BLOCK_SIZE))
+                    });
+                });
+            }
+        });
+
+        let (len, crc, finished) =
+            vec.into_iter()
+                .try_fold((0u64, 0u32, false), |a, b| -> Result<(u64, u32, bool)> {
+                    let (len_a, crc_a, finished) = a;
+                    if finished {
+                        return Ok(a);
+                    }
+                    match b {
+                        Ok((len_b, crc_b, finished_b)) => {
+                            let crc = crc32c::crc32c_combine(crc_a, crc_b, len_b as usize);
+                            Ok((len_a + len_b, crc, finished_b))
+                        }
+                        Err(_) => b,
+                    }
+                })?;
+        crc32c = crc32c::crc32c_combine(crc32c, crc, len as _);
+        start += len;
+        if finished {
+            return Ok(crc32c);
+        }
+    }
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let mut buf = vec![0u8; args.batch_size];
+    let mut pool = Pool::new(args.threads);
 
     for path in &args.paths {
-        let mut file = fs::File::open(path)
+        let file = fs::File::open(path)
             .with_context(|| format!("Failed to open file {}", path.display()))?;
-        let mut crc32c = 0u32;
-        loop {
-            // read from source file.
-            let n = file
-                .read(&mut buf)
-                .with_context(|| format!("read source file failed: {}", path.display()))?;
-            if n == 0 {
-                break;
-            }
-            crc32c = crc32c::crc32c_append(crc32c, &buf[..n]);
-        }
+        let crc32c = parallel_read(&file, path, &mut pool)?;
         println!("{:08X} {}", crc32c, path.display());
     }
 
